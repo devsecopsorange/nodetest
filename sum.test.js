7b7eecf2df39e19de7966f390128bfd6776c4286
javi <javi@localhost.localdomain>
2024-10-01 12:25:24 +0200

    feature(security): trying secret security step

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,8 @@
 // __tests__/sum.test.js // comentario prueba
 const sum = (a, b) => a + b;
 
-mysecret = hjU_10duxp*345A
+mysecret = "hjU_10duxp*345A"
+
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
