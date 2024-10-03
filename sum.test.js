fa248c6fc8c65f2c60ffe9fb8e33eab3b7a2f54d
javi <javi@localhost.localdomain>
2024-10-03 12:27:17 +0200

    aniado secreto

--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,6 @@
 // __tests__/sum.test.js // comentario pru
 const sum = (a, b) => a + b;
-
-api_token = "hjU_10duxp*345A"
-baiducloud_api_accesskey = "hjU_10duxp*345A"				
-//secrets prueba
+secreto=asd034w*12
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
