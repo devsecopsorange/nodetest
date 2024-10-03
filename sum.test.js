b4b3c084300ded84b22a4e44a2088419b3d20667
javi <javi@localhost.localdomain>
2024-10-03 12:27:59 +0200

    no relevante editado

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,5 @@
-// __tests__/sum.test.js // comentario pru
+// __tests__/sum.test.js // comentario prueba
 const sum = (a, b) => a + b;
-secreto=asd034w*12
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
